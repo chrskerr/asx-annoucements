636f5f028e0d91cba8051670c7d41a94523b484a
Chris Kerr <chriskerr@me.com>
2020-06-10 20:49:03 +1000

    rekeyed to use apollo graphql

--- src/App.js
+++ src/App.js
@@ -1,112 +1,45 @@
 
-import React, { useState, useEffect } from "react";
-import PropTypes from "proptypes";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner, faCheck, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
-import _ from "lodash";
+import React from "react";
+import Home from "./Home";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-client";
+import { WebSocketLink } from "apollo-link-ws";
+import { HttpLink } from "apollo-link-http";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 export default function App () {
-	const [ data, setData ] = useState([]);
-	const [ savedData, setSavedData ] = useState( JSON.parse( localStorage.getItem( "savedData" )));
-	const [ isLoading, setIsLoading ] = useState( 0 );
-	const [ onlyPriceSensitive, setOnlyPriceSensitive ] = useState( true );
-	const [ onlyAsx300, setOnlyAsx300 ] = useState( true );
-	
-	useEffect(() => {
-		if ( !isLoading ) {
-			setIsLoading( true );
-			fetch( `https://asx-node.herokuapp.com/?price_sensitive=${ onlyPriceSensitive ? "true" : "false" }&asx300=${ onlyAsx300 ? "true" : "false" }` )
-				.then( res => res.json())
-				.then( data => setData( _.orderBy( _.map( data, el => {
-					const { id } = el;
-					return {
-						...el,
-						read: _.get( savedData, [ id, "read" ], false ),
-						saved: _.get( savedData,[ id, "saved" ], false ),
-					};
-				})), [ "read", "saved", "id" ], [ "asc", "asc", "desc" ]))
-				.catch( error => console.error( error ))
-				.finally(() => setIsLoading( false ));
-		}
-	}, [ onlyPriceSensitive, onlyAsx300 ]);
 
-	useEffect(() => {
-		if ( !savedData ) setSavedData({});
-		localStorage.setItem( "savedData", JSON.stringify( savedData ));
-	}, [ savedData ]);
+	const httpLink = new HttpLink({
+		uri: "https://quiet-river-86309.herokuapp.com/v1/graphql",
+	});
+    
+	const wsLink = new WebSocketLink({
+		uri: "wss://quiet-river-86309.herokuapp.com/v1/graphql",
+		options: {
+			reconnect: true,
+			timeout: 30000,
+		},
+	});
 
-	const dataPlusSaved = _.map( data, el => {
-		const { id } = el;
-		return {
-			...el,
-			read: _.get( savedData, [ id, "read" ], false ),
-			saved: _.get( savedData,[ id, "saved" ], false ),
-		};
+	const link = split(
+		({ query }) => {
+			const { kind, operation } = getMainDefinition( query );
+			return kind === "OperationDefinition" && operation === "subscription";
+		},
+		wsLink,
+		httpLink,
+	);
+
+	const client = new ApolloClient({
+		link,
+		cache: new InMemoryCache(),
 	});
 
 	return (
-		<div className="body">
-			<div className="header">
-				<h2>ASX Recent Announcement Feed</h2>
-				<p>A scraped collection of ASX announcements from midnight today.</p>
-			</div>
-			<div className="filters">
-				<div>
-					<label>Price sensitive only?</label>
-					<input type="checkbox" checked={ onlyPriceSensitive } onChange={ () => setOnlyPriceSensitive( !onlyPriceSensitive ) } disabled={ isLoading } />
-				</div>
-				<div>
-					<label>ASX 300 only?</label>
-					<input type="checkbox" checked={ onlyAsx300 } onChange={ () => setOnlyAsx300( !onlyAsx300 ) } disabled={ isLoading } />
-				</div>
-				<div className={ isLoading ? "loading" : "loaded" }>
-					{ isLoading ?
-						<>
-							<p>Loading</p>
-							<FontAwesomeIcon icon={ faSpinner } spin size="2x" />
-						</>
-						: 
-						<>
-							<p>Loaded</p>
-							<FontAwesomeIcon icon={ faCheck } />
-						</>
-					}
-				</div>
-			</div>
-			{ !_.isEmpty( dataPlusSaved ) && _.map( dataPlusSaved, el => <RowCard data={ el } key={ el.id } savedData={ savedData } setSavedData={ setSavedData } /> ) }
-		</div>
+		<ApolloProvider client={ client }>
+			<Home />
+		</ApolloProvider>
 	);
 }
-
-const RowCard = ({ data, savedData, setSavedData }) => {
-	const { id, description, discussionUrl, ticker, time, businessName, read, saved } = data;
-
-	const markRead = () => { if ( !saved ) setSavedData({ ...savedData, [ id ]: { saved, read: !read }}); };
-
-	return (
-		<div className={ `card ${ saved ? "saved" : "" } ${ ( !saved && read ) ? "read": "" }` }>
-			<div className="card-header">
-				<p>{ ticker } - { businessName }</p>
-				<p>{ time }</p>
-			</div>
-			<div className="card-body">
-				<a href={ discussionUrl } target="_blank" rel="noopener noreferrer" onClick={ markRead }>{ description }<FontAwesomeIcon icon={ faExternalLinkAlt } size="xs" /></a>
-				<div className="read-saved">
-					<div>
-						<label>Saved</label>
-						<input type="checkbox" checked={ saved } onChange={ () => setSavedData({ ...savedData, [ id ]: { read: false, saved: !saved }}) } />
-					</div>
-					<div>
-						<label>Read</label>
-						<input type="checkbox" checked={ read } onChange={ markRead } disabled={ saved } />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-};
-RowCard.propTypes = {
-	data: PropTypes.object,
-	savedData: PropTypes.object,
-	setSavedData: PropTypes.func,
-};
---
+++ src/Home.js
@@ -0,0 +1,130 @@
+
+import React, { useState, useEffect } from "react";
+import PropTypes from "proptypes";
+import { useSubscription } from "@apollo/react-hooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner, faCheck, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import _ from "lodash";
+import gql from "graphql-tag";
+import { parseJSON, format, formatDistanceToNow, subBusinessDays, set, formatISO } from "date-fns";
+
+const SUBSCRIPTION = gql`
+subscription SubcribeAnnouncements ( $time_before: timestamptz!, $is_price_sensitive: Boolean!, $is_asx_300: Boolean! ) {
+    announcements( where: { 
+        time: { _gte: $time_before },
+        is_price_sensitive: { _eq: $is_price_sensitive }, 
+        stock: { is_asx_300: { _eq: $is_asx_300 }}
+    }) {
+        ann_download_url
+        description
+        hotcopper_url
+        id time
+        is_price_sensitive
+        stock {
+            name ticker
+            is_asx_300 GICS
+        }
+    }
+}`;
+
+
+export default function Home () {
+	const [ savedData, setSavedData ] = useState( JSON.parse( localStorage.getItem( "savedData" )));
+	const [ is_price_sensitive, set_is_price_sensitive ] = useState( true );
+	const [ is_asx_300, set_is_asx_300 ] = useState( true );
+	const [ is_after_4pm_yesterday, set_is_after_4pm_yesterday ] = useState( true );
+
+	const time_before = is_after_4pm_yesterday ? formatISO( set( subBusinessDays( new Date(), 1 ), { hours: 16, minutes: 0, seconds: 0 })) : formatISO( 0 );
+
+	const { data, loading, error } = useSubscription( SUBSCRIPTION, { variables: { is_price_sensitive, is_asx_300, time_before }});
+
+	console.log( data, error, time_before );
+
+	const announcements = _.get( data, "announcements" );
+	const annoucementsWithSavedData = _.map( announcements, announcement => {
+		const { id } = announcement;
+		return {
+			...announcement,
+			read: _.get( savedData, [ id, "read" ], false ),
+			saved: _.get( savedData,[ id, "saved" ], false ),
+		};});
+	const sortedAnnouncements = _.orderBy( annoucementsWithSavedData, [ "read", "saved", "id" ], [ "asc", "asc", "desc" ]);
+
+	useEffect(() => {
+		if ( !savedData ) setSavedData({});
+		localStorage.setItem( "savedData", JSON.stringify( savedData ));
+	}, [ savedData ]);
+
+	return (
+		<div className="body">
+			<div className="header">
+				<h2>ASX Recent Announcement Feed</h2>
+				<p>A scraped collection of ASX announcements from midnight today.</p>
+			</div>
+			<div className="flex-box">
+				<div>
+					<label>Price sensitive only?</label>
+					<input type="checkbox" checked={ is_price_sensitive } onChange={ () => set_is_price_sensitive( !is_price_sensitive ) } disabled={ loading } />
+				</div>
+				<div>
+					<label>ASX 300 only?</label>
+					<input type="checkbox" checked={ is_asx_300 } onChange={ () => set_is_asx_300( !is_asx_300 ) } disabled={ loading } />
+				</div>
+				<div>
+					<label>After 4pm yesterday only?</label>
+					<input type="checkbox" checked={ is_after_4pm_yesterday } onChange={ () => set_is_after_4pm_yesterday( !is_after_4pm_yesterday ) } disabled={ loading } />
+				</div>
+				<div className={ `${ loading ? "loading" : "loaded" }` }>
+					{ loading ?
+						<>
+							<p>Connecting</p>
+							<FontAwesomeIcon icon={ faSpinner } spin size="2x" />
+						</>
+						: 
+						<>
+							<p>Connected</p>
+							<FontAwesomeIcon icon={ faCheck } />
+						</>
+					}
+				</div>
+			</div>
+			{ !_.isEmpty( sortedAnnouncements ) && _.map( sortedAnnouncements, el => <RowCard data={ el } key={ el.id } savedData={ savedData } setSavedData={ setSavedData } /> ) }
+		</div>
+	);
+}
+
+const RowCard = ({ data, savedData, setSavedData }) => {
+	const { id, description, hotcopper_url, stock, time, read, saved } = data;
+	const { name, ticker, GICS } = stock;
+
+	const markRead = () => { if ( !saved ) setSavedData({ ...savedData, [ id ]: { saved, read: !read }}); };
+
+	const parsedTime =  parseJSON( time );
+
+	return (
+		<div className="card">
+			<div className="card-header">
+				<p>{ ticker } - { name } - { GICS }</p>
+				<p>{ formatDistanceToNow( parsedTime ) } ago - { format( parsedTime, "d LLL @ hh:mm bbb" )} </p>
+			</div>
+			<div className="card-body">
+				<a href={ hotcopper_url } target="_blank" rel="noopener noreferrer" onClick={ markRead }>{ description }<FontAwesomeIcon icon={ faExternalLinkAlt } size="xs" /></a>
+				<div className="read-saved">
+					<div>
+						<label>Saved</label>
+						<input type="checkbox" checked={ saved } onChange={ () => setSavedData({ ...savedData, [ id ]: { read: false, saved: !saved }}) } />
+					</div>
+					<div>
+						<label>Read</label>
+						<input type="checkbox" checked={ read } onChange={ markRead } disabled={ saved } />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+RowCard.propTypes = {
+	data: PropTypes.object,
+	savedData: PropTypes.object,
+	setSavedData: PropTypes.func,
+};
